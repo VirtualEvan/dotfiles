2a75ccba34944796217794a93b9fe129e999dc5b
VirtualEvan <virtualevan@gmail.com>
2018-05-04 10:11:04 +0200

    Update 2018 - Dynamic mas2j and /bin/build.xml execution

--- Atom-JasonAS/JasonAgentSpeak-mas2j.bat
+++ Atom-JasonAS/JasonAgentSpeak-mas2j.bat
@@ -1,8 +1,6 @@
 :: File mas2j.bat
 @ECHO OFF
 
-@ECHO OFF
-
 IF [%1]==[] (
   echo File %1 not found
   exit
@@ -18,9 +16,15 @@ SET _PROJECTPATH="%~dp1\"
 cd %_CURDIR%
 
 ::%_JASONDIR%\ant\bin\ant.bat -f %_PROJECTPATH%bin\build.xml
-::java -classpath .;bin\classes;%JASONLIB%\jason.jar jason.mas2j.parser.mas2j  %1 ::<the .mas2j file [full path]>
 
-java -classpath %_JASONLIB%\ant-launcher.jar org.apache.tools.ant.launch.Launcher -e -f %1% run
+:: Get .mas2j
+for /R %_PROJECTPATH% %%F in (*.mas2j) do set mas2j=%%F
+
+:: %mas2j% -> <the .mas2j file [full path]> (This creates the \bin\build.xml)
+java -classpath .;bin\classes;%_JASONLIB%\jason.jar jason.mas2j.parser.mas2j  %mas2j%
+
+:: \bin\build.xml [full path]
+java -classpath %_JASONLIB%\ant-launcher.jar org.apache.tools.ant.launch.Launcher -e -f %_PROJECTPATH%\bin\build.xml run
 
 ::java -classpath .;bin\classes;%_JASONDIR%\lib\jason.jar;%_JASONDIR%\lib\jade.jar;%_JASONDIR%\lib\c4jason.jar;%_JASONDIR%\lib\cartago.jar;%_JASONDIR%\lib\jacamo.jar;. jason.mas2j.parser.mas2j "%~1" %2
 ::%_JASONDIR%\ant\bin\ant.bat -f %_PROJECTPATH%bin\build.xml

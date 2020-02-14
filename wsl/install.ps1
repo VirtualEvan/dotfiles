8ccd211647904d6c6630db978c2503b27b4670b6
Esteban Puentes <virtualevan@gmail.com>
2020-02-14 17:04:09 +0100

    Update install.ps1

--- wsl/install.ps1
+++ wsl/install.ps1
@@ -3,7 +3,7 @@ workflow Rename-And-Reboot {
 
   Enable-WindowsOptionalFeature -Online -NoRestart -FeatureName Microsoft-Windows-Subsystem-Linux
 
-  Restart-Computer -Confirm -Wait
+  Restart-Computer -Wait
 
   Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1804 -OutFile Ubuntu.appx -UseBasicParsing
 
